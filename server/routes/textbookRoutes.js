cdc94b586fc2125fbdecf5f9cb7a778237676468
Katliegh <Katlegokatzu@gmail.com>
2025-07-24 13:00:54 +0200

    yep

--- server/routes/textbookRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createTextbook, getTextbooks } = require("../controllers/textbookController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.post("/", protect, createTextbook);
-router.get("/", getTextbooks);
-
-module.exports = router;
